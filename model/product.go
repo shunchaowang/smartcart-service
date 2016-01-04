401702f7c310504af9f916ae70270f5761e2a6f5
Shunchao Wang <wang.2799@osu.edu>
2016-01-03 19:57:01 -0500

    Changed product id to be int.

--- model/product.go
+++ model/product.go
@@ -1,15 +1,13 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
-
 type (
 	Product struct {
-		Id    bson.ObjectId `json:"id"`
+		Id    int `json:"id"`
 		Model string        `json:"model"`
 	}
 
 	ProductType struct {
-		Id   bson.ObjectId `json:"id"`
+		Id   int `json:"id"`
 		Name string        `json:"name"`
 	}
 )
