d57e063a0a6414994d85cb93bfb72b5ae66f7f6c
rolandwz <rolandwz@live.cn>
2016-03-24 22:42:07 -0400

    new Address struct in order model

--- model/order.go
+++ model/order.go
@@ -8,21 +8,35 @@ type (
 	Order struct {
 		Id       int     `json:"id"`
 		Amount    float32 `json:"amount"`
-        Remark string `json:"remark"`
+		Remark string `json:"remark"`
 
-        CreatedTime time.Time `json:"createdTime"`
-        UpdatedTime time.Time `json:"updatedTime"`
+		CreatedTime time.Time `json:"createdTime"`
+		UpdatedTime time.Time `json:"updatedTime"`
 
-        OrderStatus OrderStatus `json:"status"`
-        Customer Customer `json:'customer'`
-        Products []Product `json:"products"`
+		OrderStatus OrderStatus `json:"status"`
+		//Customer Customer `json:'customer'`
+		Products []Product `json:"products"`
 
-        ShippingAddress Address `json:'address'`
+		ShippingAddress Address `json:'address'`
+		BillingAddress Address `json:'address'`
 	}
 
-    OrderStatus struct {
-        Id int `json:"id"`
-        Name string `json:"name"`
-    }
+	OrderStatus struct {
+		Id int `json:"id"`
+		Name string `json:"name"`
+	}
+	
+	Address struct {
+		Company         string `json:"company"`
+		FirstName       string `json:"firstname"`
+		LastName        string `json:"lastname"`
+		FullName        string `json:"fullname"`
+		Street          string `json:"street"`
+		StreetLine      string `json:"street_line2"`
+		City            string `json:"city"`
+		State           string `json:"state"`
+		PostCode        string `json:"postcode"`
+		Country         string `json:"country"`
+	}
 
 )
